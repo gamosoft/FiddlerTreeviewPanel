f2f2870c2f3d5c2d1bff76b248979cc7d4efc9f2
gamosoft_cp <gamosoft@users.noreply.github.com>
2010-05-27 20:40:32 +0000

    Cleaned up some code and fixed location of REMOVE button to avoid hardcoding the indices...

--- TreeViewPanelExtension/TreePanel.cs
+++ TreeViewPanelExtension/TreePanel.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using Fiddler;
 using System.Windows.Forms;
+using Fiddler;
 using Microsoft.Win32;
-using System.Drawing;
-using System.ComponentModel;
-
-//TODO: Allow multiple selection of nodes in TreeView (custom control)
 
 namespace TreeViewPanelExtension
 {
@@ -19,8 +11,6 @@ namespace TreeViewPanelExtension
     public class TreePanel : IFiddlerExtension
     {
         #region "Variables"
-
-        ListViewHook _lvHook = new ListViewHook();
 
         /// <summary>
         /// Holds the nodes with the sessions
@@ -124,29 +114,13 @@ namespace TreeViewPanelExtension
         }
 
         /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="sender">Sender</param>
-        /// <param name="e">EventArgs</param>
-        private void lvSessions_SizeChanged(object sender, EventArgs e)
-        {
-            //TreeView tv = FiddlerApplication.UI.pnlSessions.Controls[Constants.TreeViewName] as TreeView;
-            //if (tv != null)
-            //{
-            //    tv.Location = ((SessionListView)sender).Location;
-            //    tv.Size = ((SessionListView)sender).Size;
-            //}
-        }
-
-        /// <summary>
-        /// 
+        /// Resize the TreeView when the panel changes it size
+        /// TODO: This might not work correctly
         /// </summary>
         /// <param name="sender">Sender</param>
         /// <param name="e">EventArgs</param>
         private void pnlSessions_SizeChanged(object sender, EventArgs e)
         {
-            // TODO: This might not be correct
-            //tv.Location = ((SessionListView)sender).Location;
             tv.Height = FiddlerApplication.UI.pnlSessions.Height;
             tv.Width = FiddlerApplication.UI.pnlSessions.Width;
         }
@@ -172,20 +146,6 @@ namespace TreeViewPanelExtension
             }
         }
         
-        //private void lvSessions_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
-        //{
-        //    Session oSession = e.Item.Tag as Session;
-        //    if (oSession != null && e.IsSelected)
-        //    {
-        //        TreeNode tn = this.FindNode(oSession);
-        //        if (tn != null)
-        //            tv.SelectedNode = tn;
-        //    }
-        //    //tv.
-        //    //e.IsSelected
-        //    //throw new NotImplementedException();
-        //}
-
         /// <summary>
         /// After expanding a node, change the icon if it's a folder
         /// </summary>
@@ -215,70 +175,29 @@ namespace TreeViewPanelExtension
         }
 
         /// <summary>
-        /// When right-clicking a button, select the tree node so we get the correct context menu
+        /// Delete sessions from the TreeView
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void tv_MouseClick(object sender, MouseEventArgs e)
+        /// <param name="sender">Sender</param>
+        /// <param name="e">ToolStripItemClickedEventArgs</param>
+        void btnRemove_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
-            if (e.Button == MouseButtons.Right)
+            switch (e.ClickedItem.Text.ToLower())
             {
-                TreeNode tn = tv.GetNodeAt(e.Location);
-                if (tn != null) tv.SelectedNode = tn;
-                //if (tn != null) this.DeleteNode(tn);
+                case "all":
+                    tv.Nodes.Clear();
+                    break;
+                case "images":
+                    break;
+                case "connects":
+                    break;
+                case "non-200s":
+                    break;
+                default:
+                    // Other possible things to do here
+                    break;
             }
         }
 
-        private TreeNode FindNode(Session s)
-        {
-            foreach (TreeNode host in tv.Nodes)
-            {
-                TreeNode found = this.FindNode(s, host);
-                if (found != null) return found;
-            }
-            return null;
-        }
-
-        private TreeNode FindNode(Session s, TreeNode tn)
-        {
-            foreach (TreeNode child in tn.Nodes)
-            {
-                Session oSession = child.Tag as Session;
-                if (oSession != null && oSession.id == s.id) return child;
-            }
-            return null;
-        }
-
-        private void DeleteNode(TreeNode tn)
-        {
-            // TODO: Implement clearing of items from the treeview
-            if (tn.Parent != null)
-            {
-                TreeNode parent = tn.Parent;
-                parent.Nodes.Remove(tn);
-                if (parent.Nodes.Count == 0) this.DeleteNode(parent);
-            }
-            else
-            {
-                if (tn.Nodes.Count == 0) tv.Nodes.Remove(tn);
-            }
-        }
-
-        //private void lvSessions_Invalidated(object sender, InvalidateEventArgs e)
-        //{
-        //    foreach (ListViewItem lvItem in lvSessions.Items)
-        //    {
-        //        Session oSession = lvItem.Tag as Session;
-        //        if (oSession != null)
-        //        {
-        //        }
-        //    }
-        //}
-
-        private void btnRemoveAll_Click(object sender, EventArgs e)
-        {
-            tv.Nodes.Clear();
-        }
 
         #endregion "Methods"
 
@@ -289,45 +208,41 @@ namespace TreeViewPanelExtension
         /// </summary>
         public void OnLoad()
         {
-            //this._lvHook.Install();
-
             // Locate the original sessions listview
             lvSessions = FiddlerApplication.UI.pnlSessions.Controls[Constants.ListViewName] as SessionListView;
-
-            // lvSessions.Items = new ListViewItemCollectionWithEvents(lvSessions);
-
-            //lvSessions.ItemSelectionChanged += new ListViewItemSelectionChangedEventHandler(lvSessions_ItemSelectionChanged);
-            //lvSessions.SizeChanged += new EventHandler(lvSessions_SizeChanged);
-            // TODO: See status of deleted items
-            //lvSessions.Invalidated += new InvalidateEventHandler(lvSessions_Invalidated);
-            //lvSessions.ItemActivate += new EventHandler(lvSessions_ItemActivate);
-            //lvSessions.ItemChecked += new ItemCheckedEventHandler(lvSessions_ItemChecked);
             FiddlerApplication.UI.pnlSessions.SizeChanged += new EventHandler(pnlSessions_SizeChanged);
 
             // Create our TreeView
             tv = new TreeView();
-            //tv.Tag = new List<TreeNode>(); // Holds the list of nodes for faster lookup
-            //tv.Dock = DockStyle.Fill;
             tv.Name = Constants.TreeViewName;
             tv.ImageList = this.imageList;
             tv.Location = lvSessions.Location;
             tv.Size = lvSessions.Size;
             tv.ContextMenu = FiddlerApplication.UI.mnuSessionContext; // Assign same context menu
-            tv.MouseClick += new MouseEventHandler(tv_MouseClick);
             tv.AfterSelect += new TreeViewEventHandler(tv_AfterSelect);
             tv.AfterCollapse += new TreeViewEventHandler(tv_AfterCollapse);
             tv.AfterExpand += new TreeViewEventHandler(tv_AfterExpand);
             FiddlerApplication.UI.pnlSessions.Controls.Add(tv);
 
-            ToolStrip menuBar = FiddlerApplication.UI.Controls[4] as ToolStrip;
-            ToolStripDropDownButton btnRemove = menuBar.Items[2] as ToolStripDropDownButton;
-            btnRemove.DropDownItems[0].Click += new EventHandler(btnRemoveAll_Click);
-            //ALL
-            //Images
-            //Connects
-            //Non-200
+            // TODO: Avoid hardcoded reference to the actual button
+            foreach (Control c in FiddlerApplication.UI.Controls)
+            {
+                if (c is ToolStrip)
+                {
+                    ToolStrip menuBar = c as ToolStrip;
+                    foreach (ToolStripItem btn in menuBar.Items)
+                    {
+                        if (btn.Text.Equals(Constants.RemoveButtonText, StringComparison.CurrentCultureIgnoreCase))
+                        {
+                            ToolStripDropDownButton btnRemove = btn as ToolStripDropDownButton;
+                            btnRemove.DropDownItemClicked += new ToolStripItemClickedEventHandler(btnRemove_DropDownItemClicked);
+                        }
+                    }
 
-            // Show or hide depending on the value
+                }
+            }
+
+            // Show or hide depending on the registry value
             this.ShowHidePanel();
         }
 
@@ -341,28 +256,8 @@ namespace TreeViewPanelExtension
             int value = this.ShowPanel ? 1 : 0;
             oReg.SetValue(Constants.RegistryShowPanel, value, RegistryValueKind.DWord);
             oReg.Close();
-
-            //this._lvHook.Uninstall();
         }
         
         #endregion "IFiddlerExtension Members"
-
-
-        //private const int LVM_FIRST = 0x1000; // ListView messages
-        //private const int LVM_DELETEALLITEMS = (LVM_FIRST + 9);
-
-        //protected override void WndProc(ref Message m)
-        //{
-        //    switch (m.Msg)
-        //    {
-        //        case LVM_DELETEALLITEMS:
-        //            tv.Nodes.Clear();
-        //            break;
-        //        default:
-        //            // Nothing
-        //            break;
-        //    }
-        //    base.WndProc(ref m);
-        //}
     }
 }
